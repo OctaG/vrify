46c7967b868a23085dd41b9dff0e249c953144eb
OctaG <millennium935@icloud.com>
2021-10-30 15:04:51 -0500

    añadió componente header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
-function App() {
-  return (
-    <h1>Hello</h1>
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import Header from './components/Header.js'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#FE5857"
+    },
+    secondary: {
+      main: "#2a7fe8"
+    },
+  },
+  typography: {
+    fontFamily: 'Helvetica',
+  },
+});
+
+function App(){
+  return(
+    <ThemeProvider theme={theme}>
+      <Container disableGutters maxWidth='false'>
+        <Header/>
+      </Container>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react';
+
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+import Navbar from 'react-bootstrap/navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+export default function Header(props){
+  return(
+    <Box sx={
+      {
+        paddingLeft: 15,
+        paddingRight: 10,
+        paddingTop: 2,
+        fontFamily: "fontFamily"
+      }
+    }>
+      <Navbar collapseOnSelect sticky="top" bg="white" variant="light">
+        <Navbar.Brand>
+          <Typography sx={{color:"primary.main"}} variant='h4'>
+            vrify
+          </Typography>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link>BUSCAR</Nav.Link>
+            <Nav.Link>CÓMO FUNCIONA</Nav.Link>
+            <Nav.Link>FEEDBACK</Nav.Link>
+            <Nav.Link>LOG IN</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </Box>
+  );
+}
