691dfb67e057e204440209297d75aed6a251340e
OctaG <millennium935@icloud.com>
2021-11-01 13:08:09 -0600

    removió imports sin usar en layout Search

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,15 +2,8 @@ import React from 'react';
 
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-
-import IosShareIcon from '@mui/icons-material/IosShare';
-import SaveAltIcon from '@mui/icons-material/SaveAlt';
 
 import SearchBar from '../components/SearchBar.js'
 
